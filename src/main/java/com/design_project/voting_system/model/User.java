6c3d1fd4d02519066c168bfb3182c162ee999a0a
powsokan <powsi1997@gmail.com>
2020-04-19 22:12:55 +0530

    model added

--- src/main/java/com/design_project/voting_system/model/User.java
+++ src/main/java/com/design_project/voting_system/model/User.java
@@ -1,10 +1,23 @@
 package com.design_project.voting_system.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
 public class User {
+
+    @Id
+    private String id;
+    private String fullname;
+    private String address;
+    private String NIC_number;
+    private String ele_vol;
+    private String ele_district;
+
+    public User(){
+
+    }
 
 }
