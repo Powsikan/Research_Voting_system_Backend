c031634c62e93edb00a63d39d16f30f81475fcc0
powsokan <powsi1997@gmail.com>
2020-04-19 20:48:55 +0530

    initial commit

---
+++ src/main/java/com/design_project/voting_system/VotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.design_project.voting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/design_project/voting_system/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design_project.voting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
