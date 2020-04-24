44425ac51409837803d5e4c61523091c1fa2202b
powsokan <powsi1997@gmail.com>
2020-04-24 20:30:35 +0530

    fingerprint validation added

--- src/main/java/com/design_project/voting_system/service/UserService.java
+++ src/main/java/com/design_project/voting_system/service/UserService.java
@@ -57,12 +57,13 @@ public class UserService {
     public Object validateFingerPrint(String NICnumber, String fingerPrint) {
         User user = userRepository.findByNicNumber(NICnumber);
 
-        if (fingerPrint==user.getFingerPrint()) {
+        if (fingerPrint == user.getFingerPrint()) {
             user.setHasVoted(true);
             userRepository.save(user);
             return new ResponseEntity(HttpStatus.OK);
-        }else
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
     }
 
 
