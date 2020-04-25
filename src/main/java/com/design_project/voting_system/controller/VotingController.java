350bc2ec7c830697c4e267124f11a56501c53109
powsokan <powsi1997@gmail.com>
2020-04-25 09:48:40 +0530

    Create a voting controller and service to separate the functionality

--- src/main/java/com/design_project/voting_system/controller/VotingController.java
+++ src/main/java/com/design_project/voting_system/controller/VotingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 
 @RestController
+@RequestMapping("/vote")
 public class VotingController {
 
     @Autowired
