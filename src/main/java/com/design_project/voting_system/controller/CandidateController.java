70c14f84941b3e1bffe85280a2f13eb322fa2020
powsokan <powsi1997@gmail.com>
2020-05-07 08:27:42 +0530

    code bug fixed

--- src/main/java/com/design_project/voting_system/controller/CandidateController.java
+++ src/main/java/com/design_project/voting_system/controller/CandidateController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
