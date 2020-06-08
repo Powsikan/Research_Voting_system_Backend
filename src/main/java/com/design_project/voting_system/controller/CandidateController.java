7a0073e8cff1358ab69a2f180eeed8f894bf8ee0
powsikan <powsi1997@gmail.com>
2020-06-08 19:58:32 +0530

    file upload

--- src/main/java/com/design_project/voting_system/controller/CandidateController.java
+++ src/main/java/com/design_project/voting_system/controller/CandidateController.java
@@ -5,6 +5,7 @@ import com.design_project.voting_system.model.Candidate;
 import com.design_project.voting_system.service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,8 +19,8 @@ public class CandidateController {
     private CandidateService candidateService;
 
     @PostMapping("/add")
-    public String addCandidate(@RequestBody Candidate candidate) {
-        return candidateService.addCandidate(candidate);
+    public String addCandidate(@RequestParam MultipartFile file , @RequestBody Candidate candidate) {
+        return candidateService.addCandidate(candidate,file);
 
     }
 
--- src/main/java/com/design_project/voting_system/service/CandidateService.java
+++ src/main/java/com/design_project/voting_system/service/CandidateService.java
@@ -5,6 +5,8 @@ import com.design_project.voting_system.model.Candidate;
 import com.design_project.voting_system.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +15,17 @@ import java.util.Optional;
 public class CandidateService {
     @Autowired
     private CandidateRepository candidateRepository;
+    @Autowired
+    private FileStorageService fileStorageService;
 
+    public String addCandidate(Candidate candidate, MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
 
-    public String addCandidate(Candidate candidate) {
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
         candidateRepository.save(candidate);
         return "Candidate added";
     }
